e8a84d6ee86ab1974dfd06632e80e0cd936e0e77
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-29 17:44:36 +0530

    secure

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -25,10 +25,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSchema);
 
 app.route("/").get(function (req, res) {
@@ -41,7 +37,7 @@ app
   })
   .post(function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, foundUser) {
       if (err) {
@@ -63,7 +59,7 @@ app
   .post(function (req, res) {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password,
+      password: md5(req.body.password),
     });
 
     newUser.save(function (err) {
