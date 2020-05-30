3a1c90878e39bf5d06e80d793a9fac7ea473a46b
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-30 10:12:11 +0530

    submission

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
+  secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -140,6 +141,30 @@ app.get("/secrets", function (req, res) {
   } else {
     res.redirect("/login");
   }
+});
+
+app.get("/submit", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function (req, res) {
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 });
 
 let port = process.env.PORT;
