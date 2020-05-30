27838165888913254eab336db0803b8f40c9d380
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-30 07:46:49 +0530

    authentication

--- app.js
+++ app.js
@@ -16,18 +16,34 @@ app.use(
     extended: true,
   })
 );
+app.use(
+  session({
+    secret: "Our little Secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/secretsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.route("/").get(function (req, res) {
   res.render("home");
@@ -38,20 +54,17 @@ app
     res.render("login");
   })
   .post(function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({ email: username }, function (err, foundUser) {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.login(user, function (err) {
       if (err) {
         console.log(err);
       } else {
-        if (foundUser) {
-          bcrypt.compare(password, foundUser.password, function (err, result) {
-            if (result === true) {
-              res.render("secrets");
-            }
-          });
-        }
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
       }
     });
   });
@@ -61,24 +74,31 @@ app
     res.render("register");
   })
   .post(function (req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-      const newUser = new User({
-        email: req.body.username,
-        password: hash,
-      });
-
-      newUser.save(function (err) {
-        if (!err) {
-          res.render("secrets");
-        } else {
-          console.log(err);
-        }
-      });
+    User.register({ username: req.body.username }, req.body.password, function (
+      err,
+      user
+    ) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
     });
   });
 
 app.get("/logout", function (req, res) {
   res.redirect("/");
+});
+
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 let port = process.env.PORT;
