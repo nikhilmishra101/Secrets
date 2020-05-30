46a5ef5f1727ad286c2c47597ac7547770e77e1a
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-30 10:21:40 +0530

    Changing Layout

--- app.js
+++ app.js
@@ -136,11 +136,15 @@ app.get("/logout", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
-  }
+  User.find({ secret: { $ne: null } }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        res.render("secrets", { usersWithSecrets: foundUser });
+      }
+    }
+  });
 });
 
 app.get("/submit", function (req, res) {
