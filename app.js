359c213564c514a44e295c7ffcf0510e7598e3a3
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-30 09:46:51 +0530

    authenticating users

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ passport.use(
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
         return cb(err, user);
       });
